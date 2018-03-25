f8a067da4437185c579bfa5c62450dc942f7be79
Felipe Monteiro <felipehmsilveira@gmail.com>
2018-03-25 18:46:32 -0300

    Configuração inicial do FireBase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,31 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { SigninPage } from '../pages/signin/signin';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen,
+              afAuth: AngularFireAuth) {
+
+    afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = SigninPage;
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,54 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthService } from '../providers/auth-service/auth-service';
+
+import { SigninPage } from '../pages/signin/signin';
+import { SignupPage } from '../pages/signup/signup';
+import { ResetpasswordPage } from '../pages/resetpassword/resetpassword';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBp8fH-ypfdjNnHbK1w0DgClrV7HA6p_t0",
+  authDomain: "auth-a910a.firebaseapp.com",
+  databaseURL: "https://auth-a910a.firebaseio.com",
+  projectId: "auth-a910a",
+  storageBucket: "auth-a910a.appspot.com",
+  messagingSenderId: "275378176056"
+};
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    SignupPage,
+    ResetpasswordPage
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    SignupPage,
+    ResetpasswordPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
